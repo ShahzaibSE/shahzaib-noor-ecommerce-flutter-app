94e6a01cc3634254ed0d2632c3f5917777f638fc
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-06-02 00:54:24 +0500

    Updated

---
+++ lib/history.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class History extends StatefulWidget {
+  History({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HistoryState createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History>{
+  //
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
