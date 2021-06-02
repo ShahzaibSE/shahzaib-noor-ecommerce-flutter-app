2c87d45376dec520afed341296fe4c11134db481
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-06-02 23:04:40 +0500

    Product List widget up and ready.

--- lib/productList.dart
+++ lib/productList.dart
@@ -13,20 +13,91 @@ class ProductList extends StatefulWidget {
 class _ProductListState extends State<ProductList>{
 
   Widget buildProductCard(Product product){
+    var numberOfReviews = product.numberOfReviews;
+    var rating = product.rating;
+    var stock = product.stock;
+    var price = '\$'+product.price;
+    var quantity = product.quantity;
+    //
     return Card(
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          const SizedBox(height: 100),
-          Image(image: AssetImage(product.imageUrl), width:100, height: 100),
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Container(
+                width: 150,
+                height: 150,
+                child:Image(image: AssetImage(product.imageUrl)),
+              )
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(product.productName),
-              Icon(
-                Icons.star_rounded,
-                color: Colors.yellowAccent,
+              Container(
+                padding: EdgeInsets.only(left: 8),
+                child: Text(
+                  product.productName, 
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold
+                    )
+                )
+              ),
+              RichText(text: TextSpan(
+                  style: Theme.of(context).textTheme.bodyText1,
+                  children: [
+                    WidgetSpan(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child:  Icon(
+                          Icons.star_rounded,
+                          color: Colors.yellowAccent,
+                        ),
+                      )
+                    ),
+                    TextSpan(text: '$rating ($numberOfReviews Reviews)', style: Theme.of(context).textTheme.bodyText1)
+                  ]
+                )
+              ),
+              // RichText(text: TextSpan(
+              //     style: Theme.of(context).textTheme.bodyText1,
+              //     children: [
+              //       TextSpan(text: quantity),
+              //       TextSpan(
+              //         text: price,
+              //         style: TextStyle(
+              //           fontSize: 25,
+              //           fontWeight: FontWeight.bold,
+              //           color: Colors.purpleAccent,
+              //         )
+              //       )
+              //     ]
+              //   )
+              // )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child:Text('$stock Pieces'), 
+                    padding: const EdgeInsets.all(10)),
+                  Container(
+                    child:  Text(price, style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purpleAccent
+                            ))
+                  )
+                ]
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 8),
+                child: Text('Quantity: $quantity')
               )
             ],
           )
--- lib/products.dart
+++ lib/products.dart
@@ -1,12 +1,11 @@
 class Product {
     static List<Product> products = [
-      Product('assets/iphone.jpeg','IPhone 12', '5', '6', '20', '60', '1'),
-      Product('assets/macbook-pro-m1.jpg','Macbook Pro M1 2020', '5', '6', '20', '60', '1'),
-      Product('assets/macbook-air-m1.jpeg','Macbook Air 2020', '5', '6', '20', '60', '1'),
-      Product('assets/gaming-pc.jpeg','Gaming PC', '5', '6', '20', '60', '1'),
-      Product('assets/laptop.jpeg','Laptop', '5', '6', '20', '60', '1'),
-      Product('assets/graphic-card.jpeg','Graphic Card', '5', '6', '20', '60', '1'),
-      Product('assets/macbook.jpeg','Macbook Pro', '5', '6', '20', '60', '1'),
+      Product('assets/iphone.jpeg','IPhone 12', '5.0', '6', '20', '60', '1'),
+      Product('assets/macbook-pro-m1.jpg','Macbook Pro M1 2020', '5.0', '6', '20', '60', '1'),
+      Product('assets/macbook-air-m1.jpeg','Macbook Air 2020', '5.0', '6', '20', '60', '1'),
+      Product('assets/gaming-pc.jpeg','Gaming PC', '5.0', '6', '20', '60', '1'),
+      Product('assets/laptop.jpeg','Laptop', '5.0', '6', '20', '60', '1'),
+      Product('assets/macbook.jpeg','Macbook Pro', '5.0', '6', '20', '60', '1'),
     ];
     String imageUrl;
     String productName;
