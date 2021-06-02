c9be07f5d080d128bad193a81a68733d72e59435
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-06-03 01:04:31 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _scaffoldKey = new GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -58,10 +59,48 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
+        leading: Builder(builder: (context) => // Ensure Scaffold is in context
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () => Scaffold.of(context).openDrawer()
+            ),
+          ),
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+                padding: EdgeInsets.zero,
+                children: const <Widget>[
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text(
+                      'Shahzaib Noor',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title: Text('Messages'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text('Profile'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                  ),
+                ],
+              ),
       ),
       body: ProductList()
        // This trailing comma makes auto-formatting nicer for build methods.
